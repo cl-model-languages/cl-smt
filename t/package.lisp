ebdd99ad576a3ec28d5ce99b5da29cc3f4e9541e
Masataro Asai <guicho2.71828@gmail.com>
2017-11-28 19:39:37 +0900

    defined impl-independent tests

--- t/package.lisp
+++ t/package.lisp
@@ -10,7 +10,8 @@
         :fiveam
         :trivia :alexandria :iterate)
   (:export
-   #:*smt-files*))
+   #:*smt-files*
+   #:*solver*))
 (in-package :cl-smt.test)
 
 (def-suite :cl-smt)
@@ -19,16 +20,69 @@
 (defvar *smt-files*
   (directory (asdf:system-relative-pathname :cl-smt #p"smt/*.*")))
 
+(defvar *solver* nil
+  "solver keyword to test, e.g. :z3")
+
 (test printer
-   (is (equal "(set-option :produce-models true)
+  (is (equal "(set-option :produce-models true)
 (set-logic QF_LIA)
 (set-logic qf_lia)
 bool
 |Bool|"
-              (with-output-to-string (s)
-                (format-smt s
-                  '((set-option :produce-models true)
-                    (set-logic ^QF_LIA)
-                    (set-logic QF_LIA)
-                    Bool
-                    |Bool|))))))
+             (with-output-to-string (s)
+               (format-smt s
+                 '((set-option :produce-models true)
+                   (set-logic ^QF_LIA)
+                   (set-logic QF_LIA)
+                   Bool
+                   |Bool|))))))
+
+(test solve
+  (is (equal `(sat
+               (model
+                (define-fun y () int 0)
+                (define-fun x () int (- 3))
+                (define-fun z () int 2)))
+             (solve '((set-option :produce-models true)
+                      (set-logic ^QF_LIA)
+                      (declare-fun x () |Int|)
+                      (declare-fun y () |Int|)
+                      (declare-fun z () |Int|)
+                      (assert (and (or
+                                    (<= (+ x 3) (* 2 y))
+                                    (>= (+ x 4) z))))
+                      (check-sat)
+                      (get-model))
+                    *solver*)))
+  #+(or)
+  (is (equal `(sat
+               (model
+                (define-fun u () int 0)
+                (define-fun x () int 0)
+                (define-fun z3name!3 () int 3)
+                (define-fun z3name!4 () int 4)
+                (define-fun y () int 0)
+                (define-fun z () int 2)
+                (define-fun v () int (- 5))))
+             
+             (solve `((set-option :produce-models true)
+                      (set-logic ^QF_LIA)
+                      (declare-fun x () |Int|)
+                      (declare-fun y () |Int|)
+                      (declare-fun z () |Int|)
+                      (declare-fun u () |Int|)
+                      (declare-fun v () |Int|)
+                      (assert
+                       (and (or (<= (+ x 3) (* 2 u))
+                                (>= (+ v 4) y)
+                                (>= (+ x y z) 2))
+                            (= 7
+                               (+ (ite (and (<= x 2) (<= 2 (+ x 3 (- 1)))) 3 0)
+                                  (ite (and (<= u 2) (<= 2 (+ u 3 (- 1)))) 4 0)))))
+                      (check-sat)
+                      (get-model))
+                    *solver*))))
+               
+
+
+  
