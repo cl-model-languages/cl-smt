c4bd120a1b7eebf88e2113bdeb44a9f96047a367
Masataro Asai <guicho2.71828@gmail.com>
2017-11-29 12:49:53 +0900

    testing yices2

--- yices2/package.lisp
+++ yices2/package.lisp
@@ -10,18 +10,18 @@
 
 ;; blah blah blah.
 
-;; (defun yices2 ()
-;;   (if (trivial-package-manager:which "yices2")
-;;       "yices2"
-;;       (asdf:system-relative-pathname :cl-smt.yices2 "yices2-1.5/builds/bin/yices2")))
-;; 
-;; (defmethod solve (input (solver-designator (eql :yices2)) &key debug)
-;;   (with-temp (d :directory t :debug debug)
-;;     (with-temp (input-file :tmpdir d :template "XXXXXX.smt" :debug debug)
-;;       (with-open-file (s input-file :direction :output :if-does-not-exist :error)
-;;         (format-smt s input))
-;;       (with-input-from-string (s (uiop:run-program `(,(yices2) "--lang" "smt" ,input-file) :output :string))
-;;         (iter (for elem in-stream s)
-;;               (collecting elem))))))
+(defun yices-smt2 ()
+  (if (trivial-package-manager:which "yices-smt2")
+      "yices-smt2"
+      (asdf:system-relative-pathname :cl-smt.yices2 "yices-2.5.4/bin/yices-smt2")))
 
-;; note: is not available at the moment
+(defmethod solve (input (solver-designator (eql :yices2)) &key debug)
+  (with-temp (d :directory t :debug debug)
+    (with-temp (input-file :tmpdir d :template "XXXXXX.smt" :debug debug)
+      (with-open-file (s input-file :direction :output :if-does-not-exist :error)
+        (format-smt s input))
+      (with-input-from-string (s (uiop:run-program `(,(yices-smt2) ,input-file) :output :string))
+        (iter (for elem in-stream s)
+              (collecting elem))))))
+
+
